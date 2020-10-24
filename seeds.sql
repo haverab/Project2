f1d414a60e0b7ac77503c9bd4d5a3f48c6277532
Allison Haver <allisonbhaver@gmail.com>
2020-10-24 12:35:45 -0400

    added seeds file

---
+++ seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO recruiter (firstName, lastName, companyName, email, password) VALUES ('Fabian', 'Byrd', 'Google', 'fbyrd@recruiter.com', 'password1');
+INSERT INTO recruiter (firstName, lastName, companyName, email, password) VALUES ('Shawanda', 'Ford', 'Amazon', 'sford@recruiter.com', 'password2');
+INSERT INTO recruiter (firstName, lastName, companyName, email, password) VALUES ('Allison', 'Haver', 'Target', 'ahaver@recruiter.com', 'password3');
+
+INSERT INTO USER (firstName, lastName, email, phone, userType)
+VALUES ('Tristan', 'Eastman', 'teastman@hireme.com', 201-555-5555, 'recent grad');
+INSERT INTO USER (firstName, lastName, email, phone, userType) 
+VALUES ('Dallis', 'Conner', 'dconner@hireme.com', 540-555-5555, 'student');
+INSERT INTO USER (firstName, lastName, email, phone, userType) 
+VALUES ('Sendy', 'Mederos', 'smederos@hireme.com', 201-555-5555, 'recent grad');
+
+INSERT INTO gigs (city, state, jobTitle)
+VALUES ('Charlotte', 'North Carolina', 'Jr. Web Developer');
+INSERT INTO gigs (city, state, jobTitle) 
+VALUES ('Charlotte', 'North Carolina', 'Front End Developer');
+INSERT INTO gigs (city, state, jobTitle)
+VALUES ('Charlotte', 'North Carolina', 'Full Stack Web Developer');

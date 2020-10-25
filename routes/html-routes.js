8f9b8da487e00ccee40847420c694341d482e6ff
Shawanda Ford <shawanda.ford@gmail.com>
2020-10-25 00:25:00 -0400

    updated html-routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -19,15 +19,14 @@ module.exports = function(app) {
 
 
 
-  app.get("/login", (req, res) => {
+  app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
-    } else {
-    
-    res.sendFile(path.join(__dirname, "../public/login.html"));
     }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
   });
+
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
